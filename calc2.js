0a9d3bc02c1868c3768f85954c04b97312476a4f
SammieCandy <samueloluyemi@icloud.com>
2022-04-27 03:02:36 +0100

    Second commit

--- calc2.js
+++ calc2.js
@@ -30,37 +30,71 @@ keys.addEventListener('click', e => {
         let keyContent = key.textContent
         let displayedNum = display.textContent
         let previousKeyType = calculator.dataset.previousKeyType
+        let defaultNum = '0'
+        
 
         Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
         if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
+            if (displayedNum === defaultNum || previousKeyType === 'operator') {
                 display.textContent = keyContent
             } else {
                 display.textContent = displayedNum + keyContent
             } 
             calculator.dataset.previousKeyType = 'number'
         }
-        if (displayedNum === 'decimal') {
-            display.textContent = displayedNum + '.'
+        if (action === 'decimal') {
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.'
+            } else  if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = '0.'
+            }
+            calculator.dataset.previousKeyType = 'decimal'
         }
+
         if (
             action === "add" ||
             action === "subtract" ||
             action === "divide" ||
-            action === "multiply" ||
-            action === "percent" 
+            action === "multiply"
         ) {
             key.classList.add('is-depressed')
             calculator.dataset.previousKeyType = 'operator'
             calculator.dataset.firstValue = displayedNum
             calculator.dataset.operator = action
         }
+        if (action === 'percent') {
+            if (key.textContent === '%') {
+                calculator.dataset.firstValue === displayedNum * (1/100)
+            } else {
+                key.textContent = '%'
+            }
+            display.textContent = displayedNum * (1/100)
+            calculator.dataset.previousKeyType = '%'
+        }
         if (action === 'delete') {
-            console.log(displayedNum.toString().slice(0, -1))
+            if(key.textContent === 'del') {
+                calculator.dataset.firstValue = displayedNum.slice(0, -1)
+                
+            } else {
+                key.textContent = 'del'
+            }
+            display.textContent = displayedNum.slice(0, -1)
+
+            calculator.dataset.previousKeyType = 'delete'
         }
+        
         if (action === 'clear') {
-            console.log('')
+            if(key.textContent === 'AC') {
+                calculator.dataset.firstValue = ''
+                calculator.dataset.operator = ''
+                calculator.dataset.secondValue = ''
+            } else {
+                key.textContent = 'AC'
+            }
+            display.textContent = defaultNum
+            calculator.dataset.previousKeyType = 'clear'
         }
+
         if (action === 'calculate') {
             console.log(calculate.dataset)
             const firstValue = calculator.dataset.firstValue
